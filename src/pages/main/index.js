0cfa00931d0a74308a09fdbc0bb3c007d258eaf2
Igor Souza <igor.souza.webmaster@gmail.com>
2019-06-29 14:47:25 -0300

    Adiciona geolocalização

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,9 +1,74 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Main = () => (
-  <>
-    <h1>Hello main!</h1>
-  </>
-);
+import ReactMapGL, { Marker, GeolocateControl } from 'react-map-gl';
+
+class Main extends Component {
+  state = {
+    viewport: {
+      width: '100%',
+      height: '97vh',
+      latitude: 0,
+      longitude: 0,
+      zoom: 15,
+    },
+    userLatitude: 0,
+    userLongitude: 0,
+  };
+
+  componentDidMount() {
+    this.handleLocateUser();
+  }
+
+  handleLocateUser = () => {
+    const { viewport } = this.state;
+
+    navigator.geolocation.getCurrentPosition((position) => {
+      const newstate = { ...viewport };
+      newstate.latitude = position.coords.latitude;
+      newstate.longitude = position.coords.longitude;
+      this.setState({
+        viewport: newstate,
+        userLatitude: newstate.latitude,
+        userLongitude: newstate.longitude,
+      });
+    }, () => {
+      const newstate = { ...viewport };
+      newstate.latitude = 40.730610;
+      newstate.longitude = -73.935242;
+      this.setState({
+        viewport: newstate,
+        userLatitude: newstate.latitude,
+        userLongitude: newstate.longitude,
+      });
+    });
+  }
+
+  render() {
+    const { viewport, userLatitude, userLongitude } = this.state;
+
+    return (
+      <>
+        <ReactMapGL
+          {...viewport}
+          onViewportChange={vp => this.setState({ viewport: vp })}
+          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        >
+          <GeolocateControl
+            positionOptions={{ enableHighAccuracy: true }}
+            trackUserLocation
+          />
+          <Marker
+            latitude={userLatitude}
+            longitude={userLongitude}
+            offsetLeft={-20}
+            offsetTop={-10}
+          >
+            <div>You are here</div>
+          </Marker>
+        </ReactMapGL>
+      </>
+    );
+  }
+}
 
 export default Main;
